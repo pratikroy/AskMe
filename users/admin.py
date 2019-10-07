c2e928dc484a97fca5d455d4fd4e87c3e24edf0a
pratikroy <pratikroy2206@gmail.com>
2019-10-07 23:04:36 +0530

    Need to complete custom user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import CustomUser
 
 # Register your models here.
+class CustomUserModel(UserAdmin):
+    model = CustomUser
+    list_display = ["username", "email", "is_staff"]
+
+
+admin.site.register(CustomUser, CustomUserModel)
