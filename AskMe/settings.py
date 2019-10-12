1c8feec05e6b07f24f6df7039a7070757c787c12
pratikroy <pratikroy2206@gmail.com>
2019-10-12 19:29:00 +0530

    Basic CSS setup for Login and Registration are done

--- AskMe/settings.py
+++ AskMe/settings.py
@@ -137,9 +137,8 @@ LOGOUT_REDIRECT_URL = "/"
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 
 # Custom user model
 AUTH_USER_MODEL = "users.CustomUser"
